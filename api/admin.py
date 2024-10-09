373e899eee6deed811bf7b611035edce16a2e13a
Hossein <hossein.gasemzadeh03@gmail.com>
2024-10-09 18:29:02 +0330

    feat: add Url model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Url
 
-# Register your models here.
+@admin.register(Url)
+class UrlAdmin(admin.ModelAdmin):
+    list_display = ['orginal_url', 'short_link', 'created']
+    list_per_page = 20
+    date_hierarchy = 'created'
--- api/models.py
+++ api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
+import random
+import string
 
-# Create your models here.
+def short_link_generetor(length:int=6):
+    
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+
+
+class Url(models.Model):
+    orginal_url = models.URLField()
+    short_link = models.CharField(max_length=10, unique=True, default=short_link_generetor)
+    
+    created = models.DateTimeField(default=timezone.now)
+    
+    
+    def __str__(self):
+        return f"{self.orginal_url} -> {self.short_link}"
+    
+    
+    class Meta:
+        ordering = ['-created']
+        
+        indexes = [
+            models.Index(fields=['-created'])
+        ]
